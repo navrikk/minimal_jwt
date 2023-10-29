978741af660c12be37719216529e8deb1504eb35
Nikshep A V <nikshep.av@gmail.com>
2023-10-30 02:28:57 +0530

    Include `MinimalJwt` module

--- lib/minimal_jwt.rb
+++ lib/minimal_jwt.rb
@@ -3,3 +3,5 @@ require 'minimal_jwt/algorithm'
 require 'minimal_jwt/errors/invalid_jwt_error'
 require 'minimal_jwt/errors/invalid_payload_error'
 require 'minimal_jwt/errors/unsupported_signing_algorithm_error'
+
+include MinimalJwt
